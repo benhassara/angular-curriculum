97f924da04e9ae25b23f134af03a8f5529a81fc7
benhassara <bhassara@gmail.com>
2015-09-21 10:06:31 -0600

    finish pig latin filter

---
+++ unit_2/03-custom-filters/js/filters.js
@@ -0,0 +1,66 @@
+angular.module('customFilters')
+  .filter('kebab', kebab)
+  .filter('camel', camel)
+  .filter('pigLatin', pigLatin);
+
+/** converts a string from snake_case to kebab-case */
+function kebab() {
+  return function(input) {
+    if (typeof input === 'string')
+      return input.replace(/_/g, '-');
+    return input;
+  };
+}
+
+/** converts snake_case or kebab-case to camelCase */
+function camel() {
+  return function(input) {
+    if (typeof input === 'string') {
+      var arr = input.split(/-|_/g);
+      if (arr.length > 1) {
+        for (var i = 1; i < arr.length; i++) {
+          arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+        }
+        return arr.join('');
+      }
+    }
+    return input;
+  };
+}
+
+/** converts a string to Pig Latin */
+function pigLatin() {
+  return function(input) {
+    if (typeof input === 'string') {
+      var words = input.split(' ');
+      words.forEach(function(word, index) {
+        words[index] = pigifyWord(word);
+      });
+      return words.join(' ');
+    }
+    return input;
+  };
+}
+
+// helpers
+
+function pigifyWord(word) {
+  var splitUp;
+  var lower = word.toLowerCase();
+  if (/[^a|e|i|o|u]/.test(lower[0]) || lower.indexOf('y') === 0) {
+    splitUp = lower.split(/([aeiou].*)/);
+    return splitUp[1] + '-' + splitUp[0] + 'ay';
+  }
+  else if (/a|e|i|o|u/.test(lower[0])) {
+    return lower + '-yay';
+  }
+  else if (lower.indexOf('y') !== -1) {
+    if (lower.length === 2 && lower.indexOf('y') === 1) {
+      return lower[1] + '-' + lower[0] + 'ay';
+    }
+    else if (/[^aeiou|\s]{2,}y/g.test(lower)) {
+      splitUp = lower.split('y');
+      return 'y' + lower[1] + '-' + lower[0] + 'ay';
+    }
+  }
+}
